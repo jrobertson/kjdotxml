578858889f9bfb5dbda4bd2c1cd437716fa752d9
James Robertson <digital.robertson@gmail.com>
2022-11-02 19:33:13 +0000

    feature: A specific verse can now be queried.

--- kjdotxml.gemspec
+++ kjdotxml.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'kjdotxml'
-  s.version = '0.1.1'
+  s.version = '0.2.0'
   s.summary = 'King James Bible in XML format i.e. KjDotXml.new.book("Genesis")'
   s.authors = ['James Robertson']
   s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.14')
   s.files = Dir["lib/kjdotxml.rb",'xml/*.xml']
+  s.add_runtime_dependency('novowels', '~> 0.1', '>=0.1.3')
   s.signing_key = '../privatekeys/kjdotxml.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/kjdotxml.rb
+++ lib/kjdotxml.rb
@@ -3,6 +3,8 @@
 # file: kjdotxml.rb
 
 require 'rexle'
+require 'abbrev'
+require 'novowels'
 
 
 BOOKS = [
@@ -78,15 +80,21 @@ class KjDotXmlError < Exception
 end
 
 class KjDotXml
+  
+  attr_reader :booklist
 
-  def initialize(title=nil)
+  def initialize(title=nil, debug: false)
+    
+    @debug = debug
+    
     book(title) if title
+    @booklist = BOOKS
+    
   end
   
   def book(s)
         
     filename = s.downcase.gsub(/\s/,'-') + '.xml'
-    #filepath = File.join('/tmp', filename)
     filepath = File.join(File.dirname(__FILE__), '..', 'xml', filename)
     
     if File.exists? filepath then
@@ -100,11 +108,78 @@ class KjDotXml
     
   end
   
-  def books()
-    BOOKS
+  def books(ref=nil)           
+    
+    return @booklist.map {|x| books(x) } unless ref
+
+    index = ref.to_s[/^\d+$/] ? (ref.to_i - 1) : find_book(ref.downcase)
+    puts 'index: ' + index.inspect if @debug
+    
+    title = @booklist[index]
+
+
   end
+
+  def verses(s)
+
+    r = s.match(/(?<book>\w+)\s+(?<chapter>\d+)[,\:]?\s*(?<verses>.*)/)
+    verses2 = r[:verses] =~ /(?:\.\.|,|-)/ ? Range.new(*r[:verses]\
+                        .split(/(?:\.\.|,|-)/).map(&:to_i)).to_a : r[:verses]
+    puts 'r[:book]: ' + r[:book].inspect if @debug
+    
+    doc = book(books(r[:book]))
+    
+    puts 'verses2: ' + verses2.inspect if @debug
+    
+    if verses2.is_a? String then
+      
+      [
+        doc.root.element("chapter[@no='#{r[:chapter]}']/verse" + 
+                         "[@no='#{verses2}']")
+      ]
+      
+    elsif verses2.is_a? Array
+      
+      verses2.map do |verse|
+        doc.root.element("chapter[@no='#{r[:chapter]}']/verse[@no='#{verse}']")
+      end
+      
+    end
+  end  
+ 
 
   def to_doc()
     @doc
   end
+  
+    private
+
+    def find_book(ref)
+
+      puts 'find_book/ref: ' + ref.inspect if @debug
+      
+      h = @booklist.inject({}) do |r,rawx|
+
+        x = rawx.downcase
+        a3 = [
+          x,
+          x.sub(/(\d)\s/,'\1'),
+          x.sub(/(\d)\s/,'\1-'),
+          NoVowels.compact(x),
+          NoVowels.compact(x.sub(/(\d)\s/,'\1')),
+          NoVowels.compact(x.sub(/(\d)\s/,'\1-')),
+        ]
+        #puts 'a3: ' + a3.inspect if @debug
+        a3b = a3.uniq.abbrev.keys.reject {|x| x[/^\s*\d+$/] or x.length < 2}
+        r.merge(rawx => a3b)
+
+      end
+      #puts 'h: '  + h.inspect if @debug
+      r = h.find {|key, val| val.grep(/#{ref}/).any? }
+      r = h.find {|key, vl| vl.grep(/#{ref.sub(/\d+\s*/,'')}/).any? } unless r
+      puts '_r: ' + r.inspect if @debug
+      @booklist.index r.first if r
+
+    end
+  
 end
